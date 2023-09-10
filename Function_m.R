02f9149f5ce346ce7105d15503ad3b180c15825c
Stefan-Maximilian-Dietrich <stefan.maximilian.dietrich@gmail.com>
2023-09-10 22:54:18 +0200

    Alpha cut geht

--- Function_m.R
+++ Function_m.R
@@ -7,35 +7,46 @@ library(ggplot2)
 library(pracma)
 
 
-product_function <- function(data, mean, sigma){
-  product <- 1
-  for(j in seq(length(data))) {
-    product <- product * dmvnorm(x  = unlist(data[j]), mean = mean, sigma = matrix(sigma))
-  }
-  return(product)
-}
-
-likelihood_function <- function(data, theta_mu, theta_sigma) {
-  likelihood <- product_function(data, mean = theta_mu, sigma = theta_sigma)
-  print(likelihood)
-  return(likelihood)
-}
 
 #################################################################################
+
+likelihood_function <- function(beta1, data) {
+  x <- as.numeric(data[,1])
+  y <- as.numeric(data[,2])
+  
+  logistic_function <- function(x, beta0, beta1) {
+    odds <- exp(beta0 + beta1 * x)
+    probability <- odds / (1 + odds)
+    return(probability)
+  }
+  
+  likelihood <- function(beta0, beta1) {
+    p <- logistic_function(x, beta0, beta1)
+    likelihood <- prod(ifelse(y == 1, p, 1 - p))
+    return(likelihood)
+  }
+  
+  beta0 <-  glm(formula = y ~ x, data = data, family = "binomial")$coefficients[1]
+  
+  result <- likelihood(beta0 = beta0, beta1 = beta1)
+  
+  return(result)
+  
+}
 
 priori_function <- function(theta, mu_priori, sigma_priori) {
   priori <- dmvnorm(x = theta, mean = mu_priori, sigma = matrix(sigma_priori)) #hier wirs nur die wahrscheilichekit des Mittelwerts betrachtet nicht die der sTREUUNG
   return(priori)
 }
 
-m_derivat_function <- function(data, mu_priori, sigma_priori, theta_mu, theta_sigma) {
-  m_derivat <- likelihood_function(data, theta_mu, theta_sigma) * priori_function(theta_mu, mu_priori, sigma_priori)
+m_derivat_function <- function(data, mu_priori, sigma_priori, theta_mu) {
+  m_derivat <- likelihood_function(data = data, beta1 = theta_mu) * priori_function(theta_mu, mu_priori, sigma_priori)
   return(m_derivat)
 }
 
-m_mu_functio <- function(data, mu_priori, sigma_priori, theta_sigma) {
+m_mu_function <- function(data, mu_priori, sigma_priori) {
   g <- function(x) {
-    return(m_derivat_function(data = data, mu_priori = mu_priori, sigma_priori = sigma_priori, theta_mu = x, theta_sigma = theta_sigma))
+    return(m_derivat_function(data = data, mu_priori = mu_priori, sigma_priori = sigma_priori, theta_mu = x))
   }
   h <- function(x) {
     return(log(g(x)))
@@ -46,7 +57,6 @@ m_mu_functio <- function(data, mu_priori, sigma_priori, theta_sigma) {
   
   x0 <- optim(par = 0, fn = h_neg, method = "BFGS")$par  #Ähnich wie Newtonverfahren 
   hII_x0 <- fderiv(f = h, x = x0, n = 2, h = 5*10^-6)
-  
   m_mu <- exp( h(x0) ) * sqrt( (2*pi) / -hII_x0) # * (pnorm(b, mean = x0, sd = sqrt(-1 / hII_x0)) - pnorm(a, mean = x0, sd = sqrt(-1 / hII_x0))) falls Parameter eingeschränkt 
     
   return(m_mu)
@@ -64,15 +74,15 @@ root_function <- function(f, intervall, step) {
   return(roots)
 }
 
-AlphaCut_mu_function <- function(alpha, mu_priori_Lower, mu_priori_upper, data, sigma_priori, theta_sigma) {
+AlphaCut_mu_function <- function(alpha, mu_priori_Lower, mu_priori_upper, data, sigma_priori) {
   m_mu_log_neg <- function(x) {
-    return(-log(m_mu_functio(data = data, mu_priori = x, sigma_priori = sigma_priori, theta_sigma = theta_sigma)))
+    return(-log(m_mu_function(data = data, mu_priori = x, sigma_priori = sigma_priori)))
   }
   max_par <- optim(par = 0, fn = m_mu_log_neg, method = "BFGS")$par  #Ähnich wie Newtonverfahren 
-  max_m <- m_mu_functio(data = data, mu_priori = max_par, sigma_priori = sigma_priori, theta_sigma = theta_sigma)
+  max_m <- m_mu_function(data = data, mu_priori = max_par, sigma_priori = sigma_priori)
  
   f <- function(x) {
-    return(m_mu_functio(data = data, mu_priori = x, sigma_priori = sigma_priori, theta_sigma = theta_sigma) - alpha * max_m)
+    return(m_mu_function(data = data, mu_priori = x, sigma_priori = sigma_priori) - alpha * max_m)
   }
   
   intervall <- c(mu_priori_Lower, mu_priori_upper)
@@ -82,10 +92,10 @@ AlphaCut_mu_function <- function(alpha, mu_priori_Lower, mu_priori_upper, data, 
   return(root)
 }
 
-utility_function <- function(a, labeld_data, unlabeled_data, theta_mu, theta_sigma, mu_priori, sigma_priori) {
+utility_function <- function(a, labeld_data, unlabeled_data, theta_mu, mu_priori, sigma_priori) {
   new_data <- c(labeld_data, unlabeled_data[a])
   fischer_info <-  - 0.25 * log(det(matrix(var(new_data))))
-  log_likelihood <- log(likelihood_function(data = new_data, theta_mu = theta_mu, theta_sigma = theta_sigma))
+  log_likelihood <- log(likelihood_function(data = new_data, beta1 = theta_mu))
   para_obs <- 1/2 * log(2*pi/length(new_data))
   log_priori <- log(priori_function(theta = theta_mu, mu_priori = mu_priori, sigma_priori = sigma_priori))
   
@@ -98,9 +108,9 @@ utility_function <- function(a, labeld_data, unlabeled_data, theta_mu, theta_sig
   return(log_likelihood + para_obs + fischer_info + log_priori)
 }
 
-expected_utility_function <- function(a, labeld_data, unlabeled_data, theta_sigma, mu_priori, sigma_priori) {
+expected_utility_function <- function(a, labeld_data, unlabeled_data, mu_priori, sigma_priori) {
   g <- function(x) {
-    return(utility_function(a = a, labeld_data = labeld_data, unlabeled_data = unlabeled_data, theta_mu = x, theta_sigma = theta_sigma, mu_priori = mu_priori, sigma_priori = sigma_priori))
+    return(utility_function(a = a, labeld_data = labeld_data, unlabeled_data = unlabeled_data, theta_mu = x, mu_priori = mu_priori, sigma_priori = sigma_priori))
   }
   
   h <- function(x) {
@@ -118,6 +128,6 @@ expected_utility_function <- function(a, labeld_data, unlabeled_data, theta_sigm
   return(expected_utility)
 }
 
-gamma_maximin_function <- function(a, labeld_data, unlabeled_data, theta_sigma, mu_priori_lower, mu_priori_upper, sigma_priori) {
+gamma_maximin_function <- function(a, labeld_data, unlabeled_data, mu_priori_lower, mu_priori_upper, sigma_priori) {
   
 }
