b4c8b05d93e946692eaf2a2707c0153f2f27bdca
Stefan-Maximilian-Dietrich <stefan.maximilian.dietrich@gmail.com>
2023-10-26 17:41:44 +0200

    Aufräumen bei den Fuktionen

--- R/Alpha_cut/Function.R
+++ R/Alpha_cut/Function.R
@@ -1,7 +1,6 @@
 #Main
 likelihood_function <- function(X, theta, response) {
   #print("likelihood_function")
-  
   Y <- response
   
   logistic_function <- function(X, theta) {
@@ -23,26 +22,17 @@ likelihood_function <- function(X, theta, response) {
   return(result)
 }
 
-priori_function <- function(theta, mu_priori, sigma_priori) {
-  #print("priori_function")
-  
-  priori <- mvnfast::dmvn(X = theta, mu = mu_priori, sigma = sigma_priori) #hier wirs nur die wahrscheilichekit des Mittelwerts betrachtet nicht die der sTREUUNG
-  return(priori)
-}
-
 utility_approximation_function <- function(X, fischer_info, response, theta, mu_priori, sigma_priori) {
   #print("utility_approximation_function")
   
   likelihood <- likelihood_function(X = X, theta = theta, response = response)
   para_obs <- 2*pi/(nrow(X) - 1)
-  priori <- priori_function(theta = theta, mu_priori = mu_priori, sigma_priori = sigma_priori)
+  priori <-  mvnfast::dmvn(X = theta, mu = mu_priori, sigma = sigma_priori)
   return(para_obs * likelihood * priori / fischer_info)
 }
 
 expected_utility_function <- function(X, fischer_info, response, mu_priori, sigma_priori) {
   #print("expected_utility_function")
-  
-  
   f <- function(x) {
     expected_utility <- utility_approximation_function(X = X, fischer_info = fischer_info, response = response, theta = x, mu_priori = mu_priori, sigma_priori = sigma_priori)
     return(expected_utility)
@@ -55,23 +45,19 @@ expected_utility_function <- function(X, fischer_info, response, mu_priori, sigm
   
   result <- utility_approximation_function(X = X, fischer_info = fischer_info, response = response, theta = arg_max_likelihood, mu_priori = mu_priori, sigma_priori = sigma_priori)
   
-  print(paste("mu_priori_b0:", format(round(mu_priori[1], 4), nsmall = 5),"mu_priori_b1:", format(round(mu_priori[2], 4), nsmall = 5), "mu_priori_b2:", format(round(mu_priori[3], 4), nsmall = 5), "mu_priori_b3:", format(round(mu_priori[4], 4), nsmall = 5),"  e_utility:", result))
+  #print(paste("mu_priori_b0:", format(round(mu_priori[1], 4), nsmall = 5),"mu_priori_b1:", format(round(mu_priori[2], 4), nsmall = 5), "mu_priori_b2:", format(round(mu_priori[3], 4), nsmall = 5), "mu_priori_b3:", format(round(mu_priori[4], 4), nsmall = 5),"  e_utility:", result))
   #print(paste("mu_priori_b0:", format(round(mu_priori[1], 5), nsmall = 5),"mu_priori_b1:", format(round(mu_priori[2], 5), nsmall = 5), "  e_utility:", result))
-  
-  
   return(result)
 } 
 
 m_derivat_function <- function(X,  response, mu_priori, sigma_priori, theta) {
   #print("m_derivat_function")
-  
-  m_derivat <- likelihood_function(X = X, theta = theta,  response = response) * priori_function(theta = theta, mu_priori = mu_priori, sigma_priori = sigma_priori)
+  m_derivat <- likelihood_function(X = X, theta = theta,  response = response) * mvnfast::dmvn(X = theta, mu = mu_priori, sigma = sigma_priori) 
   return(m_derivat)
 }
 
 m_mu_function <- function(X,  response, mu_priori, sigma_priori) {
   #print("m_mu_function")
-  
   g <- function(x) {
     return(m_derivat_function(X = X, response = response, mu_priori = mu_priori, sigma_priori = sigma_priori, theta = x))
   }
@@ -83,8 +69,7 @@ m_mu_function <- function(X,  response, mu_priori, sigma_priori) {
   }
   
   start <- rep(0, times = ncol(X))
-  x0 <- optim(par = start, fn = h_neg, method = "BFGS")$par  #Ähnich wie Newtonverfahren 
-  
+  x0 <- optim(par = start, fn = h_neg, method = "BFGS")$par  #Ähnich wie Newtonverfahren
   hII_x0 <- hessian(h, x0)
   
   m_mu <- exp( h(x0) ) * sqrt( (2*pi)^2 / abs(det(hII_x0))) # * (pnorm(b, mean = x0, sd = sqrt(-1 / hII_x0)) - pnorm(a, mean = x0, sd = sqrt(-1 / hII_x0))) falls Parameter eingeschränkt 
@@ -93,8 +78,6 @@ m_mu_function <- function(X,  response, mu_priori, sigma_priori) {
 
 m_alpha_function <- function(X,  response , mu_priori , sigma_priori, alpha, m_max) {
   #print("m_alpha_function")
-  
-  
   result <-  m_mu_function(X = X,  response = response, mu_priori = mu_priori, sigma_priori = sigma_priori) - m_max * alpha
   
   return(result)
@@ -102,7 +85,6 @@ m_alpha_function <- function(X,  response , mu_priori , sigma_priori, alpha, m_m
 
 m_max_alpha_function <- function(X,  response , sigma_priori, mu_priori_lower, mu_priori_upper) {
   #print("m_alpha_function")
-  
   fn <- function(x) {
     result <- - m_mu_function(X = X, response = response, mu_priori = x, sigma_priori = sigma_priori) 
     return(result)
@@ -116,10 +98,7 @@ m_max_alpha_function <- function(X,  response , sigma_priori, mu_priori_lower, m
 
 gamma_maximin_alpaC_function <- function(X, fischer_info, response, mu_priori_lower, mu_priori_upper, sigma_priori, alpha) {
   #print("gamma_maximin_alpaC_function")
-  
   m_max <- m_max_alpha_function(X = X, response = response , sigma_priori = sigma_priori, mu_priori_lower = mu_priori_lower, mu_priori_upper = mu_priori_upper)
-  print(m_max)
-  
   
   expected_utility <- function(x) {
     result <- expected_utility_function(X = X, fischer_info = fischer_info, response = response, mu_priori = x, sigma_priori = sigma_priori)
@@ -150,7 +129,6 @@ gamma_maximin_alpaC_addapter <- function(data, glm_formula, target, mu_priori_lo
   data_matrix <- as.matrix(selected_column <- subset(data, select = pred_variables))
   X <- cbind(1, data_matrix)
   fischer_info <- sqrt((det(var(data_matrix))))
-  
   response <- as.matrix(selected_column <- subset(data, select = target))
   
   result <- gamma_maximin_alpaC_function(X = X, fischer_info = fischer_info, response = response, mu_priori_lower = mu_priori_lower, mu_priori_upper = mu_priori_upper, sigma_priori = sigma_priori, alpha = alpha)$objective
