b7dca88c23dafdf58ac5e134c2b307698c150da5
Stefan-Maximilian-Dietrich <stefan.maximilian.dietrich@gmail.com>
2023-11-02 15:50:45 +0100

    alpha bei banknoten

--- Testreihe_alpha.R
+++ Testreihe_alpha.R
@@ -1,8 +1,23 @@
+#Simuliert
 seq <- seq(from = 0.05, to = 0.95, by = 0.05)
 res <- c()
 for(a in seq) {
-  source(paste(getwd(),"/R/Alpha_cut/benchmark-alpha-cut.R", sep = ""))
+  print("### a ###")
+  source(paste(getwd(),"/benchmarks/run_benchmarks_simulated_data_multi_model_p=6.R", sep = ""))
   load(paste(getwd(),"/results/alpaha_cut_",share_unlabeled,"_",data, "_n=", as.character(n), "_p=", as.character(p), "_a=", as.character(alpha), sep=""))
   res <- c(res,saved_results$`Inductive on-the-fly mean`[length(saved_results$`Inductive on-the-fly mean`)])
   View(res)
 }
+
+#Banknoten
+seq <- seq(from = 0.05, to = 0.95, by = 0.05)
+res <- c()
+for(a in seq) {
+  print("### a ###")
+  source(paste(getwd(),"/benchmarks/run_benchmarks_banknote.R", sep = ""))
+  load(paste(getwd(),"/results/alpaha_cut_",share_unlabeled,"_",data, "_n=", as.character(n), "_p=", as.character(p), "_a=", as.character(alpha), sep=""))
+  res <- c(res,saved_results$`Inductive on-the-fly mean`[length(saved_results$`Inductive on-the-fly mean`)])
+  View(res)
+}
+
+
