2cb1e6eda3ed3259e3e925944a4e69d58c6003f3
Stefan-Maximilian-Dietrich <stefan.maximilian.dietrich@gmail.com>
2025-03-22 13:23:45 +0100

    jetzt läuft

--- NaiveBayes/run_benchmark_nb.R
+++ NaiveBayes/run_benchmark_nb.R
@@ -1,12 +1,12 @@
 #load Functions
-source("~/Soft_Revision/NaiveBayes/Function_NB.R")
+source(paste(getwd(),"/NaiveBayes/Function_NB.R", sep = ""))
 
 
 #load Data
-source("~/Soft_Revision/NaiveBayes/data_NB/banknote.R")
+source(paste(getwd(),"/NaiveBayes/data_NB/banknote.R", sep = ""))
 
-N = 4
-for(n_labled in c(4,6,8,10,12)) { 
+N = 60
+for(n_labled in c(4)) { 
   for(n_unlabled in c(2,4,8,16,32,64,128)) {
     for(alpha in c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)) {
       print(paste("RUNNING: n_labled: ", n_labled, " n_unlabled: ", n_unlabled, " alpha: ", alpha, sep= ""))
@@ -18,7 +18,7 @@ for(n_labled in c(4,6,8,10,12)) {
       
       gamma <- foreach(m = 1:N, .combine = 'c') %dopar% {
         set.seed(m+n_labled*100+n_unlabled*10000+alpha*100000)
-        source("~/Soft_Revision/NaiveBayes/NB.R")
+        source(paste(getwd(),"/NaiveBayes/NB.R", sep = ""))
       }
       
       path = paste(getwd(),"/NaiveBayes/results_NB/", data_name,"_L_", as.character(n_labled), "_U_",  as.character(n_unlabled), "_alp_" , as.character(alpha), sep="")
