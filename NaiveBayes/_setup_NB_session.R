2129ca18c7fa65f77490640883dc0a3706c652bd
Stefan-Maximilian-Dietrich <stefan.maximilian.dietrich@gmail.com>
2025-03-22 13:11:06 +0100

    alles sortiert

--- NaiveBayes/_setup_NB_session.R
+++ NaiveBayes/_setup_NB_session.R
@@ -1,6 +1,14 @@
 install.packages("naivebayes")
 install.packages("caret")
+install.packages("tidyverse")
+install.packages("doParallel")
 
 
 library("naivebayes")	
 library("caret")
+library("tidyverse")
+library("doParallel")
+
+
+### Data Sets
+install.packages("MixGHD")
--- NaiveBayes/run_benchmark_nb.R
+++ NaiveBayes/run_benchmark_nb.R
@@ -1,35 +1,27 @@
-data(banknote)
-data_frame <- banknote %>% as.data.frame()
-names(data_frame)[names(data_frame) == 'Status'] <- 'target'
+#load Functions
+source("~/Soft_Revision/NaiveBayes/Function_NB.R")
 
-name_df = "banknote" # for results 
-data = "banknote"
-# formula for glm
-formula = target ~  Diagonal+ Bottom + Length #Length + Left + Right + Bottom + Top + Diagonal    
-target = "target" 
-data_frame[c(target)] <- data_frame[c(target)] %>% unlist() %>% as.factor()
-levels_present <- levels(data_frame[c(target)] %>% unlist())
 
-data = data_frame
-formula = target ~ Diagonal + Bottom + Length
+#load Data
+source("~/Soft_Revision/NaiveBayes/data_NB/banknote.R")
 
-N = 60
+N = 4
 for(n_labled in c(4,6,8,10,12)) { 
   for(n_unlabled in c(2,4,8,16,32,64,128)) {
     for(alpha in c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)) {
       print(paste("RUNNING: n_labled: ", n_labled, " n_unlabled: ", n_unlabled, " alpha: ", alpha, sep= ""))
       
-      core <- as.numeric(min(20, parallel::detectCores() , N)) #
+      core <- as.numeric(min(20, parallel::detectCores() , N)) 
       print(paste("Parallel: cores ", core))
       cl <- parallel::makeForkCluster(core)
       doParallel::registerDoParallel(cl)
       
       gamma <- foreach(m = 1:N, .combine = 'c') %dopar% {
         set.seed(m+n_labled*100+n_unlabled*10000+alpha*100000)
-        source("NB.R")
+        source("~/Soft_Revision/NaiveBayes/NB.R")
       }
       
-      path = paste(getwd(),"/NB_eadmissible/", "experiment_","n_labled_", as.character(n_labled), "_n_unlabled_",  as.character(n_unlabled), "_alpha_" , as.character(alpha), sep="")
+      path = paste(getwd(),"/NaiveBayes/results_NB/", data_name,"_L_", as.character(n_labled), "_U_",  as.character(n_unlabled), "_alp_" , as.character(alpha), sep="")
       save(gamma, file = path)
       parallel::stopCluster(cl)
       
@@ -38,13 +30,3 @@ for(n_labled in c(4,6,8,10,12)) {
   }
   
 }
-
-
-path = paste(getwd(),"/NB_eadmissible/", "experiment_","n_labled_", as.character(4), "_n_unlabled_",  as.character(2), "_alpha_" , as.character(0.1), sep="")
-
-load(path)
-gamma 
-
-n_labled = 4
-n_unlabled = 8
-alpha = 0.6
