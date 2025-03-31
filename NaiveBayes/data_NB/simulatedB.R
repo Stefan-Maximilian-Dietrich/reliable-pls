86fbc47fbb32c9bce35bb0f702b6f26a76fa7061
Stefan-Maximilian-Dietrich <stefan.maximilian.dietrich@gmail.com>
2025-03-31 02:50:37 +0200

    simuliert B

---
+++ NaiveBayes/data_NB/simulatedB.R
@@ -0,0 +1,17 @@
+
+categories <- 1:5
+varibales <- 60
+data  <- NULL
+n <- 3000
+for(i in categories) {
+  for(j in 1:n){
+    X <-rnorm(varibales, categories[i], 2)
+    point <- c(i, X)
+    data <- rbind(data, point)
+  }
+}
+
+colnames(data) <- c("target", paste0("Var", 1:varibales))
+formula  <- as.formula(paste("target ~", paste(paste0("Var", 1:varibales), collapse = " + ")))  # Formel als String
+data <- as.data.frame(data)
+data$target <- as.factor(data$target)
