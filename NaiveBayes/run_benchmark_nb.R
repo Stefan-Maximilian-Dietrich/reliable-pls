957f1be9b3cb33ffae37fe4b1e272cc34027bf8b
Stefan-Maximilian-Dietrich <stefan.maximilian.dietrich@gmail.com>
2025-04-15 15:11:39 +0200

    korrektur

--- NaiveBayes/run_benchmark_nb.R
+++ NaiveBayes/run_benchmark_nb.R
@@ -3,16 +3,16 @@ source(paste(getwd(),"/NaiveBayes/Function_NB.R", sep = ""))
 source(paste(getwd(),"/NaiveBayes/Methods.R", sep = ""))
 
 
-label <- c(4)
-unlabel <- c(130)
+label <- c(6)
+unlabel <- c(20)
 alp <- c(0.9)
 N = 4
-used_data <- c("banknote")
+used_data <- c("brestAll_cancer")
 
 for(dat in used_data) {
   data_loader(dat)
   levels_present <- levels(data[,c(all.vars(formula)[1])]) 
-  prioris <- generate_priori_simplex(levels_present, step = 0.005)
+  prioris <- generate_priori_simplex(levels_present, step = 0.1)
   for(n_labled in label) { 
     for(n_unlabled in unlabel) {
       for(alpha in alp) {
@@ -25,15 +25,15 @@ for(dat in used_data) {
         
         gamma <- foreach(m = 1:N, .combine = 'c') %dopar% {
           set.seed(m+n_labled*100+n_unlabled*10000)
-          #set.seed(m+n_labled*100+n_unlabled*10000+alpha*100000)
+          set.seed(m+n_labled*100+n_unlabled*10000+alpha*100000)
           
           sample <- sampler_NB_up(n_labled,n_unlabled,data, formula)
           train <- sample[[1]]
           unlabeld <- sample[[2]]
           test <-sample[[3]]
-          profvis({
-            e_admissible <- e_admissible_SSL(prioris, train, unlabeld, test, alpha)
-          })
+        
+          e_admissible <- e_admissible_SSL(prioris, train, unlabeld, test, alpha)
+        
           SSL <- refernce_SSL(train, unlabeld, test, priori = NULL)
           SL <- refernce_SL(train, unlabeld, test, priori = NULL) 
           
@@ -51,6 +51,11 @@ for(dat in used_data) {
   }
 }
 
+
+
+
+
+################################################################################
 n_labled <- c(20)
 n_unlabled <- c(120)
 alpha <- c(0.8)
@@ -76,6 +81,13 @@ alpha <- c(0.8)
 dat <- c("banknote")
 m = 2
 ## gut 
+
+n_labled <- c(20)
+n_unlabled <- c(120)
+alpha <- c(0.8)
+dat <- c("iris")
+m = 2
+## iris 
 
 n_labled <- c(20)
 n_unlabled <- c(170)
