652217e6e73ff9bce8207df106875903b6501258
Stefan-Maximilian-Dietrich <102241712+Stefan-Maximilian-Dietrich@users.noreply.github.com>
2025-09-26 01:40:22 +0200

    - ff

--- NeuronalNet/data/test_possible.R
+++ NeuronalNet/data/test_possible.R
@@ -1,10 +1,11 @@
 A <- ave_accuracy_matrix(labled = 2:4, hidden = 1:5, dat = "Banknote", N = 4, workers = 4, metric = "Accuracy")
-B <- ave_accuracy_matrix(labled = 2:12, hidden = 1:6, dat = "Breast_Cancer", N = 16, workers = 4, metric = "Accuracy")
-C <- ave_accuracy_matrix(labled = 3:6, hidden = 1:5, dat = "Iris", N = 8, workers = 4, metric = "Accuracy")
+B <- ave_accuracy_matrix(labled = 2:40, hidden = 1:20, dat = "Breast_Cancer", N = 10, workers = 4, metric = "Accuracy")
+C <- ave_accuracy_matrix(labled = 3:30, hidden = 1:10, dat = "Iris", N = 24, workers = 4, metric = "Accuracy")
+D <- ave_accuracy_matrix(labled = 2:100, hidden = 1:25, dat = "Circle", N = 50, workers = 4, metric = "Accuracy")
 
 
 library(reshape2)
-df <- melt(C)
+df <- melt(D)
 
 # Heatmap mit ggplot2
 library(ggplot2)
@@ -14,4 +15,4 @@ ggplot(df, aes(x = hidden, y = labled, fill = value)) +
   scale_y_reverse() +            # damit "oben" auch oben ist
   labs(x = "hidden", y = "labled", fill = "Accuracy") +
   theme_minimal()
-
+ 
